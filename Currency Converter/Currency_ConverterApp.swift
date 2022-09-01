24b63aeffdbec31de5ca01789a837680d0351b51
NSJoker <laughofnsjoker@gmail.com>
2022-09-01 19:25:01 +0530

    Initial Commit

---
+++ Currency Converter/Currency_ConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Currency_ConverterApp.swift
+//  Currency Converter
+//
+//  Created by Chandrachudh K on 01/09/22.
+//
+
+import SwiftUI
+
+@main
+struct Currency_ConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
